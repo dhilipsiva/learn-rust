7ff877bb4753089b988a69e25ef98b6675f4098f
dhilipsiva <dhilipsiva@pm.me>
2021-08-28 21:10:45 +0530

    compare

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -8,6 +9,15 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("failed to read from command line");
+    let guess: i32 = guess
+        .trim()
+        .parse()
+        .expect("Please enter a number to parse");
+    match guess.cmp(&secret_number) {
+        Ordering::Equal => println!("Correct"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Less => println!("Too small"),
+    }
     println!("The number you entered is: {}", guess);
     println!("The number I guessed is: {}", secret_number);
 }
