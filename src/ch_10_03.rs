a2dc187f3217114b38ca0c8fbc51f60b2838d692
dhilipsiva <dhilipsiva@pm.me>
2022-09-30 12:52:37 +0530

    minor bugfix

--- src/ch_10_03.rs
+++ src/ch_10_03.rs
@@ -8,10 +8,7 @@ fn longest<'a>(x: &'a String, y: &'a String) -> &'a String {
 
 pub fn ch_10_03() {
     let string1 = String::from("abcd");
-    let result;
-    {
-        let string2 = String::from("efdddd");
-        result = longest(&string1, &string2);
-    }
+    let string2 = String::from("efdddd");
+    let result = longest(&string1, &string2);
     println!("The longest string is {}", result);
 }
