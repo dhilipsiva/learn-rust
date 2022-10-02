c14fcfca1d11b52c67104802f336831f78d8833b
dhilipsiva <dhilipsiva@pm.me>
2022-10-03 00:08:04 +0530

    closures

---
+++ src/ch_13_01.rs
@@ -0,0 +1,56 @@
+use std::thread;
+
+#[derive(Debug)]
+enum ShirtColor {
+    Red,
+    Blue,
+}
+
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+
+impl Inventory {
+    fn giveaway(&self, user_preferece: Option<ShirtColor>) -> ShirtColor {
+        user_preferece.unwrap_or_else(|| self.most_stocked())
+    }
+
+    fn most_stocked(&self) -> ShirtColor {
+        let mut red_count = 0;
+        let mut blue_count = 0;
+        for shirt in &self.shirts {
+            match shirt {
+                ShirtColor::Red => red_count += 1,
+                ShirtColor::Blue => blue_count += 1,
+            }
+        }
+        if red_count > blue_count {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
+}
+
+pub fn ch_13_01() {
+    let inventory = Inventory {
+        shirts: vec![ShirtColor::Red, ShirtColor::Blue, ShirtColor::Red],
+    };
+    let user_preferece = Some(ShirtColor::Blue);
+    let giveaway1 = inventory.giveaway(user_preferece);
+    dbg!(giveaway1);
+
+    let giveaway2 = inventory.giveaway(None);
+    dbg!(giveaway2);
+
+    let mut list = vec![1, 2, 3];
+    dbg!(&list);
+
+    let mut borrow_mutably = || list.push(5);
+    borrow_mutably();
+    dbg!(&list);
+
+    thread::spawn(move || println!("From thread: {:?}", list))
+        .join()
+        .unwrap();
+}
