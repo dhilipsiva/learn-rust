eee0dedfa553f43fae1ac6823073610cf3065b18
dhilipsiva <dhilipsiva@pm.me>
2022-10-04 05:00:06 +0530

    implement a con list

--- src/ch_15_01.rs
+++ src/ch_15_01.rs
@@ -20,6 +20,21 @@ fn basic_box() {
     // dbg!(&b);
 }
 
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+fn recurse_box() {
+    let list = List::Cons(
+        1,
+        Box::new(List::Cons(2, Box::new(List::Cons(3, Box::new(List::Nil))))),
+    );
+    dbg!(&list);
+}
+
 pub fn ch_15_01() {
     basic_box();
+    recurse_box();
 }
