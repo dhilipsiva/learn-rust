3295ad28502d19c7f01d7650b38e9e4277bbc89e
dhilipsiva <dhilipsiva@pm.me>
2022-10-05 23:11:54 +0530

    assert results

--- src/ch_15_01.rs
+++ src/ch_15_01.rs
@@ -6,16 +6,13 @@ fn basic_box() {
     //    println!("`b` is 5")
     // }
     let i = *b; // dereferencing
-    if i == 5 {
-        println!("`b` is 5")
-    }
+    assert_eq!(i, 5);
     dbg!(&b);
 
     let b = Box::new(String::from("my box"));
     let s = *b; // dereferencing
-    if s == String::from("my box") {
-        println!("`s` is {s}")
-    }
+    assert_eq!(s, String::from("my box"));
+
     // uncommenting the following will fail becuase of move; does not implement Copy trait
     // dbg!(&b);
 }
@@ -34,7 +31,15 @@ fn recurse_box() {
     dbg!(&list);
 }
 
+fn deref_examples() {
+    let x = 5;
+    let y = Box::new(x);
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
 pub fn ch_15_01() {
     basic_box();
     recurse_box();
+    deref_examples();
 }
